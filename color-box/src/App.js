063f51007580aebeba07b219063f2fbce2984b54
Malte Hoch <mhoch94@gmail.com>
2021-10-19 17:10:04 +0200

    React.js: Color Boxes

---
+++ color-box/src/App.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect, useContext} from 'react'
+import './App.css'
+
+/* ----------------------------- Global --------------------------- */
+
+const boxes = 32; 
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+/* --------------------------------------------------------------- */
+
+export default function App(){ 
+    
+    const drawBoxes = () => {
+        let n = 0;
+        let drawBoxes = [];
+        while (n < boxes) {
+            drawBoxes.push(
+            <ColorBox color={{getRandomColor}} />
+            );
+            n++;
+        }
+        return drawBoxes;
+    }
+
+    return (
+    <div className="App">
+        {drawBoxes()}
+    </div>
+    )
+}
+
+export function ColorBox() {
+
+    const [color, setColor] = useState()
+
+    const clickHandler = () => {
+        setColor(getRandomColor())
+    }
+
+    useEffect(() => { // Render boxes on init
+        clickHandler()
+    }, []) // only once
+
+    return (
+        <div className="color-box" 
+             onClick={clickHandler}
+             style={{backgroundColor: color}}/>
+    )
+
+}
---
+++ color-box/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* ___________________ Components ___________________ */
+import App from './App';
+/* __________________________________________________ */
+
+/* _____________________ Styles _____________________ */
+//
+/* __________________________________________________ */
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
